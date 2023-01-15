8aaac09aaeccd21ab26f9d09a3e5d75f2259d055
artem <gyumyush228@gmail.com>
2023-01-15 21:44:07 +0300

    added the ability to block column updates and copy the text title

--- app.js
+++ app.js
@@ -1,14 +1,25 @@
 const cols = document.querySelectorAll('.col');
 
-document.addEventListener('keydown', event => {
+document.addEventListener('keydown', (event) => {
+    event.preventDefault();
     if (event.code.toLowerCase() === 'space'){
         setRandomColors();
     }
 });
 
-document.addEventListener('click', event =>{
-    console.log(event.target.dataset)
-    const type = event.target.dataset;
+document.addEventListener('click', (event) =>{
+    const type = event.target.dataset.type
+
+    if(type === 'lock'){
+        const node = event.target.tagName.toLowerCase() === 'i'
+            ? event.target
+            : event.target.children[0]
+
+        node.classList.toggle('fa-lock-open');
+        node.classList.toggle('fa-lock');
+    } else if (type === 'copy') {
+        copyToClickboard(event.target.textContent);
+    }
 });
 
 function regenerateRandomColor(){
@@ -21,11 +32,20 @@ function regenerateRandomColor(){
     return '#' + color;
 };
 
+function copyToClickboard(text) {
+    return navigator.clipboard.writeText(text);
+}
+
 function setRandomColors() {
     cols.forEach((col) =>{
+        const isLocked = col.querySelector('i').classList.contains('fa-lock');
         const text = col.querySelector('h2');
         const button = col.querySelector('button');
         const color = chroma.random();
+
+        if(isLocked) {
+            return;
+        }
 
         text.textContent = color;
         col.style.background = color;
